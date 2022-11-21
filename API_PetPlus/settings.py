7f306ece5e83439eebffc9cdd1407a901b1e425b
lucasvasquezr21 <luc.vasquezr@duocuc.cl>
2022-11-21 17:57:02 -0300

    cambios en settings

--- API_PetPlus/settings.py
+++ API_PetPlus/settings.py
@@ -27,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-o6^b!3w8k0i6x280@kvig)lz$*%jjzp@2mbhahx@(ygxi5j-j#'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ["*"]
 
